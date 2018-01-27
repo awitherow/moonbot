cf075e0feb82461c96c4c28f7c4e24ac8c8e4f21
awitherow <au.witherow@gmail.com>
2018-01-27 11:06:29 -0500

    add changes for finding new coins and modifying cmc db

--- src/cmc.py
+++ src/cmc.py
@@ -1,36 +1,60 @@
 #!/usr/bin/python
 """ the cmc package servers as a coinmarketcap adapter """
 
+from operator import itemgetter
+
 import coinmarketcap
+import helpers
 import postgres
 
 TICKER_GETTER = coinmarketcap.Market()
 
 
+def store_tickers(new):
+    """ stores all coinmarketcap tickers from the last hour """
+    # simply store all tickers
+    postgres.wipe_cmc_history()
+
+    for ticker in new:
+        postgres.add_cmc_data(ticker)
+
+
 def get_all_coins():
     """ get all coin tickers from coinmarketcap """
 
-    all_tickers = TICKER_GETTER.ticker(limit=0)
-    past_tickers = postgres.get_past_tickers()
+    new = TICKER_GETTER.ticker(limit=0)
+    past = postgres.get_past_tickers()
 
-    # TODO: first -> check ticker lengths, if equal
-    # - if past_tickers is empty, return from entire function.
-    # - find "new" based on id comparison
-    # - send message to channel about new coin
+    new = sorted(new, key=itemgetter("rank"), reverse=True)
+    past = sorted(past, key=itemgetter("rank"), reverse=True)
 
-    # sort all tickers by rank
-    # compare one by one of each in past_tickers
-    # TODO: second -> if 1) rank has increased by X%, add to list of increased ranking percentage
-    # - what X% is good to catch changes in top 10 that are "huge",
-    # - but also in top 100, top 250, etc that are "important"
-    # - also what is the limit? do we check all?
-    # - perhaps scaling X, 1-10, 10-25, 25-100, 100-250, 250+?
-    # TODO: third -> using same rules above, check percentages for 2) hourly 3) daily, 4) weekly percentage changes
-    # TODO: fourth ->: using same rules above, check 5) usd fluctuations.
-    # once rank change complete, sent out a message with each of these listed out.
+    if past is not None:
+        if len(past) == len(all):
+            new_coins = []
 
-    # TODO: fifth ->
-    # if same tickers are in more than 2 (or 3?) of the 5 above, create special
-    # announcement how this one was in each of the list groups "high attention coins"
+            for tick in past:
+                match = helpers.find(new, "id", tick["id"])
 
-    # TODO: first -> after all, for each ticker in all_tickers, store to past_tickers if updated.
+                if match is None:
+                    new_coins.append(match)
+
+            # if len(new_coins):
+                # send message of new coins on CMC
+
+            # compare one by one of each in past
+            # TODO: second -> if 1) rank has increased by X%, add to list of increased ranking percentage
+            # - what X% is good to catch changes in top 10 that are "huge",
+            # - but also in top 100, top 250, etc that are "important"
+            # - also what is the limit? do we check all?
+            # - perhaps scaling X, 1-10, 10-25, 25-100, 100-250, 250+?
+            # TODO: third -> using same rules above, check percentages for 2) hourly 3) daily, 4) weekly percentage changes
+            # TODO: fourth ->: using same rules above, check 5) usd fluctuations.
+            # once rank change complete, sent out a message with each of these listed out.
+
+            # TODO: fifth ->
+            # if same tickers are in more than 2 (or 3?) of the 5 above, create special
+            # announcement how this one was in each of the list groups "high attention coins"
+
+            # TODO: first -> after all, for each ticker in all_tickers, store to past if updated.
+
+    store_tickers(new)
